38f35f02611f72c531d4992bc4d6dd7891947a86
BeermannCode <kralle@doombox.local>
2026-03-04 20:18:48 +0100

    🦅 BeermannCode[ac9ddc23]: Implementiere eine Command-Registry und einen Fallback für unbekannte Befehle in

---
+++ app.py
@@ -0,0 +1,18 @@
+import sys
+
+commands = {
+    'hello': lambda: print('Hello, world!'),
+    'goodbye': lambda: print('Goodbye, world!')
+}
+
+def handle_command(command):
+    if command in commands:
+        commands[command]()
+    else:
+        print(f'Unknown command: {command}')
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        handle_command(sys.argv[1])
+    else:
+        print('No command provided')
